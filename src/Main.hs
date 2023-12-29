708a58f8e7b6a85d0cb0764e88473f178251be1f
Satoshi Ogata <satosystems@gmail.com>
2023-12-29 19:20:56 +0900

    Format Haskell code by hindent

--- src/Main.hs
+++ src/Main.hs
@@ -11,9 +11,11 @@ import TextShow
 
 import qualified Monomer.Lens as L
 
-newtype AppModel = AppModel {
-  _clickCount :: Int
-} deriving (Eq, Show)
+newtype AppModel =
+  AppModel
+    { _clickCount :: Int
+    }
+  deriving (Eq, Show)
 
 data AppEvent
   = AppInit
@@ -22,40 +24,42 @@ data AppEvent
 
 makeLenses 'AppModel
 
-buildUI
-  :: WidgetEnv AppModel AppEvent
-  -> AppModel
-  -> WidgetNode AppModel AppEvent
-buildUI wenv model = widgetTree where
-  widgetTree = vstack [
-      label "Hello world",
-      spacer,
-      hstack [
-        label $ "Click count: " <> showt (model ^. clickCount),
-        spacer,
-        button "Increase count" AppIncrease
-      ]
-    ] `styleBasic` [padding 10]
+buildUI ::
+     WidgetEnv AppModel AppEvent -> AppModel -> WidgetNode AppModel AppEvent
+buildUI wenv model = widgetTree
+  where
+    widgetTree =
+      vstack
+        [ label "Hello world"
+        , spacer
+        , hstack
+            [ label $ "Click count: " <> showt (model ^. clickCount)
+            , spacer
+            , button "Increase count" AppIncrease
+            ]
+        ] `styleBasic`
+      [padding 10]
 
-handleEvent
-  :: WidgetEnv AppModel AppEvent
+handleEvent ::
+     WidgetEnv AppModel AppEvent
   -> WidgetNode AppModel AppEvent
   -> AppModel
   -> AppEvent
   -> [AppEventResponse AppModel AppEvent]
-handleEvent wenv node model evt = case evt of
-  AppInit -> []
-  AppIncrease -> [Model (model & clickCount +~ 1)]
+handleEvent wenv node model evt =
+  case evt of
+    AppInit -> []
+    AppIncrease -> [Model (model & clickCount +~ 1)]
 
 main :: IO ()
 main = do
   startApp model handleEvent buildUI config
   where
-    config = [
-      appWindowTitle "Hello world",
-      appWindowIcon "./assets/images/icon.png",
-      appTheme darkTheme,
-      appFontDef "Regular" "./assets/fonts/Roboto-Regular.ttf",
-      appInitEvent AppInit
+    config =
+      [ appWindowTitle "Hello world"
+      , appWindowIcon "./assets/images/icon.png"
+      , appTheme darkTheme
+      , appFontDef "Regular" "./assets/fonts/Roboto-Regular.ttf"
+      , appInitEvent AppInit
       ]
     model = AppModel 0
